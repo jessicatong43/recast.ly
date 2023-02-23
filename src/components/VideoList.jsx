2267029c0ed0c7afd90b65286d122f69cffd36e5
Jessica <jessicatong43@gmail.com>
2023-02-22 16:51:10 -0800

    Finished BMRs with Kyle Stevens over the last two days. Committing for record keeping purposes.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos, setVideo}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map((video) => (
+      <VideoListEntry key={video.etag} video={video} setVideo={setVideo} />
+    ))}
   </div>
 );
 
